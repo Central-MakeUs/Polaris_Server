62438375197b7aaeeb15618005084a1a62e1467a
kimmjieun <tttyyy0101@naver.com>
2021-07-10 17:48:45 +0900

    fix : 회원가입&로그인 validation 수정, 불필요한 코드 삭제

--- src/main/java/com/example/dobit/src/user/UserInfoController.java
+++ src/main/java/com/example/dobit/src/user/UserInfoController.java
@@ -84,13 +84,18 @@ public class UserInfoController {
      * @return BaseResponse<PostLoginRes>
      */
     @PostMapping("/users/login")
-    public BaseResponse<PostLoginRes> login(@RequestBody PostLoginReq postLoginReq) {
+    public BaseResponse<PostLoginRes> login(@RequestBody PostLoginReq postLoginReq) throws BaseException {
         if (postLoginReq.getEmail() == null || postLoginReq.getEmail().length() == 0) {
             return new BaseResponse<>(EMPTY_EMAIL);
         }
         if (!isRegexEmail(postLoginReq.getEmail())) {
             return new BaseResponse<>(INVALID_EMAIL);
         }
+        boolean existEmail =  userInfoProvider.retrieveEmail(postLoginReq.getEmail());
+        if(existEmail){
+            return new BaseResponse<>(EXIST_EMAIL);
+        }
+
         if (postLoginReq.getPassword() == null || postLoginReq.getPassword().length() == 0) {
             return new BaseResponse<>(EMPTY_PASSWORD);
         }
@@ -265,171 +270,5 @@ public class UserInfoController {
         }
     }
 
-//    /**
-//     * 회원 전체 조회 API
-//     * [GET] /users
-//     * 회원 닉네임 검색 조회 API
-//     * [GET] /users?word=
-//     * @return BaseResponse<List<GetUsersRes>>
-//     */
-//    @ResponseBody
-//    @GetMapping("") // (GET) 127.0.0.1:9000/users
-//    public BaseResponse<List<GetUsersRes>> getUsers(@RequestParam(required = false) String word) {
-//        try {
-//            List<GetUsersRes> getUsersResList = userInfoProvider.retrieveUserInfoList(word);
-//            if (word == null) {
-//                return new BaseResponse<>(BaseResponseStatus.SUCCESS_READ_USERS, getUsersResList);
-//            } else {
-//                return new BaseResponse<>(BaseResponseStatus.SUCCESS_READ_SEARCH_USERS, getUsersResList);
-//            }
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
-//
-//    /**
-//     * 회원 조회 API
-//     * [GET] /users/:userId
-//     * @PathVariable userId
-//     * @return BaseResponse<GetUserRes>
-//     */
-//    @ResponseBody
-//    @GetMapping("/{userId}")
-//    public BaseResponse<GetUserRes> getUser(@PathVariable Integer userId) {
-//        if (userId == null || userId <= 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_USERID);
-//        }
-//
-//        try {
-//            GetUserRes getUserRes = userInfoProvider.retrieveUserInfo(userId);
-//            return new BaseResponse<>(BaseResponseStatus.SUCCESS_READ_USER, getUserRes);
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
-//
-//    /**
-//     * 회원가입 API
-//     * [POST] /users
-//     * @RequestBody PostUserReq
-//     * @return BaseResponse<PostUserRes>
-//     */
-//    @ResponseBody
-//    @PostMapping("")
-//    public BaseResponse<PostUserRes> postUsers(@RequestBody PostUserReq parameters) {
-//        // 1. Body Parameter Validation
-//        if (parameters.getEmail() == null || parameters.getEmail().length() == 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_EMAIL);
-//        }
-//        if (!isRegexEmail(parameters.getEmail())){
-//            return new BaseResponse<>(BaseResponseStatus.INVALID_EMAIL);
-//        }
-//        if (parameters.getPassword() == null || parameters.getPassword().length() == 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_PASSWORD);
-//        }
-//        if (parameters.getConfirmPassword() == null || parameters.getConfirmPassword().length() == 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_CONFIRM_PASSWORD);
-//        }
-//        if (!parameters.getPassword().equals(parameters.getConfirmPassword())) {
-//            return new BaseResponse<>(BaseResponseStatus.DO_NOT_MATCH_PASSWORD);
-//        }
-//        if (parameters.getNickname() == null || parameters.getNickname().length() == 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_NICKNAME);
-//        }
-//
-//        // 2. Post UserInfo
-//        try {
-//            PostUserRes postUserRes = userInfoService.createUserInfo(parameters);
-//            return new BaseResponse<>(BaseResponseStatus.SUCCESS_POST_USER, postUserRes);
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
-//
-//    /**
-//     * 회원 정보 수정 API
-//     * [PATCH] /users/:userId
-//     * @PathVariable userId
-//     * @RequestBody PatchUserReq
-//     * @return BaseResponse<PatchUserRes>
-//     */
-//    @ResponseBody
-//    @PatchMapping("/{userId}")
-//    public BaseResponse<PatchUserRes> patchUsers(@PathVariable Integer userId, @RequestBody PatchUserReq parameters) {
-//        if (userId == null || userId <= 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_USERID);
-//        }
-//
-//        if (!parameters.getPassword().equals(parameters.getConfirmPassword())) {
-//            return new BaseResponse<>(BaseResponseStatus.DO_NOT_MATCH_PASSWORD);
-//        }
-//
-//        try {
-//            return new BaseResponse<>(BaseResponseStatus.SUCCESS_PATCH_USER, userInfoService.updateUserInfo(userId, parameters));
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
-//
-//    /**
-//     * 로그인 API
-//     * [POST] /users/login
-//     * @RequestBody PostLoginReq
-//     * @return BaseResponse<PostLoginRes>
-//     */
-//    @PostMapping("/login")
-//    public BaseResponse<PostLoginRes> login(@RequestBody PostLoginReq parameters) {
-//        // 1. Body Parameter Validation
-//        if (parameters.getEmail() == null || parameters.getEmail().length() == 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_EMAIL);
-//        } else if (!isRegexEmail(parameters.getEmail())) {
-//            return new BaseResponse<>(BaseResponseStatus.INVALID_EMAIL);
-//        } else if (parameters.getPassword() == null || parameters.getPassword().length() == 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_PASSWORD);
-//        }
-//
-//        // 2. Login
-//        try {
-//            PostLoginRes postLoginRes = userInfoProvider.login(parameters);
-//            return new BaseResponse<>(BaseResponseStatus.SUCCESS_LOGIN, postLoginRes);
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
-//
-//    /**
-//     * 회원 탈퇴 API
-//     * [DELETE] /users/:userId
-//     * @PathVariable userId
-//     * @return BaseResponse<Void>
-//     */
-//    @DeleteMapping("/{userId}")
-//    public BaseResponse<Void> deleteUsers(@PathVariable Integer userId) {
-//        if (userId == null || userId <= 0) {
-//            return new BaseResponse<>(BaseResponseStatus.EMPTY_USERID);
-//        }
-//
-//        try {
-//            userInfoService.deleteUserInfo(userId);
-//            return new BaseResponse<>(BaseResponseStatus.SUCCESS_DELETE_USER);
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
-//
-//    /**
-//     * JWT 검증 API
-//     * [GET] /users/jwt
-//     * @return BaseResponse<Void>
-//     */
-//    @GetMapping("/jwt")
-//    public BaseResponse<Void> jwt() {
-//        try {
-//            int userId = jwtService.getUserId();
-//            userInfoProvider.retrieveUserInfo(userId);
-//            return new BaseResponse<>(BaseResponseStatus.SUCCESS_JWT);
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>(exception.getStatus());
-//        }
-//    }
+
 }
--- src/main/java/com/example/dobit/src/user/UserInfoProvider.java
+++ src/main/java/com/example/dobit/src/user/UserInfoProvider.java
@@ -30,7 +30,6 @@ public class UserInfoProvider {
      * @throws BaseException
      */
     public UserInfo retrieveUserInfoByEmail(String email) throws BaseException {
-        // 1. email을 이용해서 UserInfo DB 접근
         List<UserInfo> existsUserInfoList;
         try {
             existsUserInfoList = userInfoRepository.findByEmailAndStatus(email, "ACTIVE");
@@ -38,7 +37,6 @@ public class UserInfoProvider {
             throw new BaseException(FAILED_TO_FIND_BY_EMAIL_AND_STATUS);
         }
 
-        // 2. 존재하는 UserInfo가 있는지 확인
         UserInfo userInfo;
         if (existsUserInfoList != null && existsUserInfoList.size() > 0) {
             userInfo = existsUserInfoList.get(0);
@@ -46,7 +44,6 @@ public class UserInfoProvider {
             throw new BaseException(NOT_FOUND_USER);
         }
 
-        // 3. UserInfo를 return
         return userInfo;
     }
 
@@ -57,10 +54,8 @@ public class UserInfoProvider {
      * @throws BaseException
      */
     public PostLoginRes login(PostLoginReq postLoginReq) throws BaseException {
-        // 1. DB에서 email로 UserInfo 조회
         UserInfo userInfo = retrieveUserInfoByEmail(postLoginReq.getEmail());
 
-        // 2. UserInfo에서 password 추출
         String password;
         try {
             password = new AES128(Secret.USER_INFO_PASSWORD_KEY).decrypt(userInfo.getPassword());
@@ -68,15 +63,12 @@ public class UserInfoProvider {
             throw new BaseException(FAILED_TO_DECRYPT_PASSWORD);
         }
 
-        // 3. 비밀번호 일치 여부 확인
         if (!postLoginReq.getPassword().equals(password)) {
             throw new BaseException(WRONG_PASSWORD);
         }
 
-        // 3. Create JWT
         String jwt = jwtService.createJwt(userInfo.getUserIdx());
 
-        // 4. PostLoginRes 변환하여 return
         int userIdx = userInfo.getUserIdx();
         return new PostLoginRes(userIdx, jwt);
     }
@@ -138,129 +130,4 @@ public class UserInfoProvider {
     }
 
 
-//    /**
-//     * 전체 회원 조회
-//     * @return List<UserInfoRes>
-//     * @throws BaseException
-//     */
-//    public List<GetUsersRes> retrieveUserInfoList(String word) throws BaseException {
-//        // 1. DB에서 전체 UserInfo 조회
-//        List<UserInfo> userInfoList;
-//        try {
-//            if (word == null) { // 전체 조회
-//                userInfoList = userInfoRepository.findByStatus("ACTIVE");
-//            } else { // 검색 조회
-//                userInfoList = userInfoRepository.findByStatusAndNicknameIsContaining("ACTIVE", word);
-//            }
-//        } catch (Exception ignored) {
-//            throw new BaseException(BaseResponseStatus.FAILED_TO_GET_USER);
-//        }
-//
-//        // 2. UserInfoRes로 변환하여 return
-//        return userInfoList.stream().map(userInfo -> {
-//            int id = userInfo.getId();
-//            String email = userInfo.getEmail();
-//            return new GetUsersRes(id, email);
-//        }).collect(Collectors.toList());
-//    }
-//
-//    /**
-//     * 회원 조회
-//     * @param userId
-//     * @return UserInfoDetailRes
-//     * @throws BaseException
-//     */
-//    public GetUserRes retrieveUserInfo(int userId) throws BaseException {
-//        // 1. DB에서 userId로 UserInfo 조회
-//        UserInfo userInfo = retrieveUserInfoByUserId(userId);
-//
-//        // 2. UserInfoRes로 변환하여 return
-//        int id = userInfo.getId();
-//        String email = userInfo.getEmail();
-//        String nickname = userInfo.getNickname();
-//        String phoneNumber = userInfo.getPhoneNumber();
-//        return new GetUserRes(id, email, nickname, phoneNumber);
-//    }
-//
-//    /**
-//     * 로그인
-//     * @param postLoginReq
-//     * @return PostLoginRes
-//     * @throws BaseException
-//     */
-//    public PostLoginRes login(PostLoginReq postLoginReq) throws BaseException {
-//        // 1. DB에서 email로 UserInfo 조회
-//        UserInfo userInfo = retrieveUserInfoByEmail(postLoginReq.getEmail());
-//
-//        // 2. UserInfo에서 password 추출
-//        String password;
-//        try {
-//            password = new AES128(Secret.USER_INFO_PASSWORD_KEY).decrypt(userInfo.getPassword());
-//        } catch (Exception ignored) {
-//            throw new BaseException(BaseResponseStatus.FAILED_TO_LOGIN);
-//        }
-//
-//        // 3. 비밀번호 일치 여부 확인
-//        if (!postLoginReq.getPassword().equals(password)) {
-//            throw new BaseException(BaseResponseStatus.WRONG_PASSWORD);
-//        }
-//
-//        // 3. Create JWT
-//        String jwt = jwtService.createJwt(userInfo.getId());
-//
-//        // 4. PostLoginRes 변환하여 return
-//        int id = userInfo.getId();
-//        return new PostLoginRes(id, jwt);
-//    }
-//
-//    /**
-//     * 회원 조회
-//     * @param userId
-//     * @return UserInfo
-//     * @throws BaseException
-//     */
-//    public UserInfo retrieveUserInfoByUserId(int userId) throws BaseException {
-//        // 1. DB에서 UserInfo 조회
-//        UserInfo userInfo;
-//        try {
-//            userInfo = userInfoRepository.findById(userId).orElse(null);
-//        } catch (Exception ignored) {
-//            throw new BaseException(BaseResponseStatus.FAILED_TO_GET_USER);
-//        }
-//
-//        // 2. 존재하는 회원인지 확인
-//        if (userInfo == null || !userInfo.getStatus().equals("ACTIVE")) {
-//            throw new BaseException(BaseResponseStatus.NOT_FOUND_USER);
-//        }
-//
-//        // 3. UserInfo를 return
-//        return userInfo;
-//    }
-//
-//    /**
-//     * 회원 조회
-//     * @param email
-//     * @return UserInfo
-//     * @throws BaseException
-//     */
-//    public UserInfo retrieveUserInfoByEmail(String email) throws BaseException {
-//        // 1. email을 이용해서 UserInfo DB 접근
-//        List<UserInfo> existsUserInfoList;
-//        try {
-//            existsUserInfoList = userInfoRepository.findByEmailAndStatus(email, "ACTIVE");
-//        } catch (Exception ignored) {
-//            throw new BaseException(BaseResponseStatus.FAILED_TO_GET_USER);
-//        }
-//
-//        // 2. 존재하는 UserInfo가 있는지 확인
-//        UserInfo userInfo;
-//        if (existsUserInfoList != null && existsUserInfoList.size() > 0) {
-//            userInfo = existsUserInfoList.get(0);
-//        } else {
-//            throw new BaseException(BaseResponseStatus.NOT_FOUND_USER);
-//        }
-//
-//        // 3. UserInfo를 return
-//        return userInfo;
-//    }
 }
